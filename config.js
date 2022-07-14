0a571add267249c90102e9a160ad93623f632e30
C4ptainFoxy <captainf0xygamingpro@gmail.com>
2022-07-15 01:34:09 +0200

    3rd

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     "registercommands" : false,
-     "token": "OTIzMjU1ODYxMTU2MTIyNjI0.G4DzBc.1uA9uNExdhP35ZAf7Cs0zonmV8Pid6ueSCZYdc",
+     "token": "",
     "imageapi": "bb03f373caa534fcfcbaeae177a65134f44a6e57ba7a7b098be273867b376d8a677ddae3c23c6ded4fec8288573945e8c3483689deb13f229376ad4b5b60231d",
     "ownerID": ["697791647227969598"],
 "prefix": "$",
